e1892baa013bb423b38994914ca7be54d159a324
Dawson Booth <dawsonbooth@Dawsons-MacBook-Pro.local>
2019-11-19 01:33:33 -0600

    Move Redux logic to separate folder

--- App.tsx
+++ App.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import DrawerContainer from './src/navigation/DrawerContainer';
-
-
-const initialState: State = {
-  count: 0
-}
-const reducer = (state: State = initialState, action: Action) => {
-  switch (action.type) {
-    case 'INCREASE_COUNTER':
-      return {
-        count: state.count + 1
-      }
-    case 'DECREASE_COUNTER':
-      return {
-        count: state.count - 1
-      }
-  }
-  return state;
-}
-const store = createStore(reducer);
+import { Provider } from 'react-redux';
+import { store } from './src/redux';
 
 export default function App() {
-
   return (
     <Provider store={store}>
       <DrawerContainer />
