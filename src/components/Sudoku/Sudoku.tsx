2ba2283ec23698e660cd3599f465f5c2ebc62687
dawsonbooth <github@dawsonbooth.com>
2019-12-31 23:43:22 -0600

    Extract useGame hook

--- src/components/Sudoku/Sudoku.tsx
+++ src/components/Sudoku/Sudoku.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-import { Alert } from "react-native";
-import _ from "lodash";
-import { useScreenDimensions } from "./hooks";
+import React from "react";
+import { useGame, useScreenDimensions } from "./hooks";
 import defaultColors, { ColorsContext } from "./colors";
 import defaultSettings, { SettingsContext } from "./settings";
 import { Col, Grid } from "native-base";
@@ -20,53 +18,20 @@ export default function _Sudoku({
     colors = defaultColors,
     settings = defaultSettings
 }: PropTypes) {
-    const [game, setGame] = useState(Game.load(board) || Game.new(9, 0.4));
-    const [notesMode, setNotesMode] = useState(false);
+    const {
+        game,
+        notesMode,
+        handleCellPress,
+        handleNotesButtonPress,
+        handleEraserButtonPress,
+        handleRevealButtonPress,
+        handleNumberButtonPress
+    } = useGame(board);
+
     const { height, width } = useScreenDimensions();
     const boardSize = Math.min(height * 0.5, width);
 
     const controlsSize = width;
-
-    const handleCellPress = (location: Sudoku.Location) => {
-        game.select(location);
-        setGame(_.clone(game));
-        // TODO: Find out why this is necessary
-    };
-
-    const handleNotesButtonPress = () => {
-        setNotesMode(!notesMode);
-    };
-
-    const handleEraserButtonPress = () => {
-        game.erase();
-        setGame(_.clone(game));
-    };
-
-    const handleRevealButtonPress = () =>
-        Alert.alert(
-            "Reveal",
-            "Are you sure you want to reveal this cell?",
-            [
-                {
-                    text: "Cancel",
-                    style: "cancel"
-                },
-                {
-                    text: "OK",
-                    onPress: () => {
-                        game.reveal();
-                        setGame(_.clone(game));
-                    }
-                }
-            ],
-            { cancelable: false }
-        );
-
-    const handleNumberButtonPress = (number: number) => {
-        if (notesMode) game.toggleNote(number);
-        else game.write(number);
-        setGame(_.clone(game));
-    };
 
     return (
         <ColorsContext.Provider value={colors}>
--- src/components/Sudoku/hooks.ts
+++ src/components/Sudoku/hooks.ts
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Alert, ScaledSize } from "react-native";
+import Game from "./Game";
+import _ from "lodash";
 
 export const useScreenDimensions = () => {
     const [screenData, setScreenData] = useState(Dimensions.get("screen"));
 
     useEffect(() => {
-        const onChange = result => {
+        const onChange = (result: {
+            window: ScaledSize;
+            screen: ScaledSize;
+        }): void => {
             setScreenData(result.screen);
         };
 
@@ -16,5 +21,60 @@ export const useScreenDimensions = () => {
 
     return {
         ...screenData
+    };
+};
+
+export const useGame = (board: Sudoku.Game["board"]) => {
+    const [game, setGame] = useState(Game.load(board) || Game.new(9, 0.4));
+    const [notesMode, setNotesMode] = useState(false);
+
+    const handleCellPress = (location: Sudoku.Location): void => {
+        game.select(location);
+        setGame(_.clone(game)); // necessary because of Object.is comparison
+    };
+
+    const handleNotesButtonPress = (): void => {
+        setNotesMode(!notesMode);
+    };
+
+    const handleEraserButtonPress = (): void => {
+        game.erase();
+        setGame(_.clone(game));
+    };
+
+    const handleRevealButtonPress = (): void =>
+        Alert.alert(
+            "Reveal",
+            "Are you sure you want to reveal this cell?",
+            [
+                {
+                    text: "Cancel",
+                    style: "cancel"
+                },
+                {
+                    text: "OK",
+                    onPress: () => {
+                        game.reveal();
+                        setGame(_.clone(game));
+                    }
+                }
+            ],
+            { cancelable: false }
+        );
+
+    const handleNumberButtonPress = (number: number): void => {
+        if (notesMode) game.toggleNote(number);
+        else game.write(number);
+        setGame(_.clone(game));
+    };
+
+    return {
+        game,
+        notesMode,
+        handleCellPress,
+        handleNotesButtonPress,
+        handleEraserButtonPress,
+        handleRevealButtonPress,
+        handleNumberButtonPress
     };
 };
