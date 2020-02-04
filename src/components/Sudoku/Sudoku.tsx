6864be1437497f1ffccd6d89a398505dba95bb30
dawsonbooth <github@dawsonbooth.com>
2020-02-03 21:54:37 -0600

    Save board on change

--- src/components/Sudoku/Sudoku.tsx
+++ src/components/Sudoku/Sudoku.tsx
@@ -9,59 +9,61 @@ import Board from "./Board";
 import Controls from "./Controls";
 
 interface PropTypes {
-    board?: Sudoku.Game["board"];
-    settings?: Sudoku.Settings;
-    colors?: Sudoku.Colors;
+  board?: Sudoku.Game["board"];
+  onChange?: (board: Sudoku.Game["board"]) => void;
+  settings?: Sudoku.Settings;
+  colors?: Sudoku.Colors;
 }
 
 export default function _Sudoku({
-    board = null,
-    colors = defaultColors,
-    settings = defaultSettings
+  board = null,
+  onChange = () => {},
+  colors = defaultColors,
+  settings = defaultSettings
 }: PropTypes) {
-    const {
-        game,
-        notesMode,
-        handleCellPress,
-        handleNotesButtonPress,
-        handleEraserButtonPress,
-        handleRevealButtonPress,
-        handleNumberButtonPress
-    } = useGame(() =>
-        board === null
-            ? Game.new(settings.degree, settings.prefilledRatio)
-            : Game.load(board)
-    );
+  const {
+    game,
+    notesMode,
+    handleCellPress,
+    handleNotesButtonPress,
+    handleEraserButtonPress,
+    handleRevealButtonPress,
+    handleNumberButtonPress
+  } = useGame(onChange, () =>
+    board === null
+      ? Game.new(settings.degree, settings.prefilledRatio)
+      : Game.load(board)
+  );
 
-    const { height, width } = useScreenDimensions();
-    const boardSize = Math.min(height * 0.5, width);
+  const { height, width } = useScreenDimensions();
+  const boardSize = Math.min(height * 0.5, width);
 
-    const controlsSize = width;
+  const controlsSize = width;
 
-    return (
-        <ColorsContext.Provider value={colors}>
-            <SettingsContext.Provider value={settings}>
-                <Grid>
-                    <Col style={{ alignItems: "center" }}>
-                        <Board
-                            grid={game.board}
-                            handleCellPress={handleCellPress}
-                            size={boardSize}
-                        />
-                        <Controls
-                            progress={game.progress}
-                            size={controlsSize}
-                            notesMode={notesMode}
-                            handleNotesButtonPress={handleNotesButtonPress}
-                            handleEraserButtonPress={handleEraserButtonPress}
-                            handleRevealButtonPress={handleRevealButtonPress}
-                            handleNumberButtonPress={handleNumberButtonPress}
-                        />
-                    </Col>
-                </Grid>
-            </SettingsContext.Provider>
-        </ColorsContext.Provider>
-    );
+  return (
+    <ColorsContext.Provider value={colors}>
+      <SettingsContext.Provider value={settings}>
+        <Grid>
+          <Col style={{ alignItems: "center" }}>
+            <Board
+              grid={game.board}
+              handleCellPress={handleCellPress}
+              size={boardSize}
+            />
+            <Controls
+              progress={game.progress}
+              size={controlsSize}
+              notesMode={notesMode}
+              handleNotesButtonPress={handleNotesButtonPress}
+              handleEraserButtonPress={handleEraserButtonPress}
+              handleRevealButtonPress={handleRevealButtonPress}
+              handleNumberButtonPress={handleNumberButtonPress}
+            />
+          </Col>
+        </Grid>
+      </SettingsContext.Provider>
+    </ColorsContext.Provider>
+  );
 }
 
 _Sudoku.Game = Game;
