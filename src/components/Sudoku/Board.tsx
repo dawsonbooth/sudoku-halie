c51fd2b3fefe1374dffc6b61a1feb0df5b7af4d9
dawsonbooth <github@dawsonbooth.com>
2020-02-04 15:23:22 -0600

    Fix borders

--- src/components/Sudoku/Board.tsx
+++ src/components/Sudoku/Board.tsx
@@ -1,38 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View } from "react-native";
 import { Grid, Row, Col } from "react-native-easy-grid";
 import Cell from "./Cell";
+import { ColorsContext } from "./colors";
 
 interface PropTypes {
-    grid: Sudoku.Game["board"];
-    handleCellPress: Function;
-    size: number;
+  grid: Sudoku.Game["board"];
+  handleCellPress: Function;
+  size: number;
 }
 
 export default function Board({ grid, handleCellPress, size }: PropTypes) {
-    return (
-        <View style={{ height: size, width: size, padding: 0.05 * size }}>
-            <Grid>
-                {grid.map((row: Sudoku.Cell[], r: Sudoku.Location["row"]) => (
-                    <Row key={r}>
-                        {row.map(
-                            (cell: Sudoku.Cell, c: Sudoku.Location["col"]) => (
-                                <Col key={c}>
-                                    <Cell
-                                        row={r}
-                                        column={c}
-                                        {...cell}
-                                        onPress={() =>
-                                            handleCellPress({ row: r, col: c })
-                                        }
-                                        boardSize={size}
-                                    />
-                                </Col>
-                            )
-                        )}
-                    </Row>
-                ))}
-            </Grid>
-        </View>
-    );
+  const colors = useContext(ColorsContext);
+
+  return (
+    <View
+      style={{
+        height: size,
+        width: size,
+        padding: 0.05 * size
+      }}
+    >
+      <Grid style={{ backgroundColor: colors.board.border, borderWidth: 2 }}>
+        {grid.map((row: Sudoku.Cell[], r: Sudoku.Location["row"]) => (
+          <Row key={r}>
+            {row.map((cell: Sudoku.Cell, c: Sudoku.Location["col"]) => (
+              <Col key={c}>
+                <Cell
+                  row={r}
+                  column={c}
+                  {...cell}
+                  onPress={() => handleCellPress({ row: r, col: c })}
+                  boardSize={size}
+                />
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </Grid>
+    </View>
+  );
 }
