350e81dc71f75005fea5820c7da2cdd6fd4c274d
dawsonbooth <github@dawsonbooth.com>
2020-07-08 23:06:27 -0500

    Use useTheme hook for Game colors

--- TODO.md
+++ TODO.md
@@ -1,12 +1,11 @@
-## Home Screen
-- [ ] Find name (Simpl(e/y) Sudoku)
+## Home
+- [ ] Find name (Sudokhue)
 - [ ] Design Home screen
 
 ## New Game
 - [ ] Only vertical slider (like brightness/volume) that changes difficulty (and colors)
 
 ## Game
-- [ ] Use useTheme hook for theme colors in Game screen
 - [ ] Use sudoku-tools
 - [ ] Add undo and redo buttons to game
 
--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -2,55 +2,58 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import Header from "../components/Header";
 import Sudoku from "../sudoku";
-import { Layout } from "@ui-kitten/components";
+import { Layout, useTheme } from "@ui-kitten/components";
 import { useGame, useSettings } from "../redux";
 import i18n from "i18n-js";
 import { NewGameButton, SettingsButton } from "../navigation/buttons";
 
-const lightColors = {
-  text: "#101426",
-  board: {
-    border: "#101426",
-    cell: {
-      normal: "#FFFFFF",
-      peer: "#C7E2FF",
-      equal: "#94CBFF",
-      conflict: "#FFA8B4",
-      selected: "#FFF1C2",
-      completed: "#B3FFD6",
-    },
-  },
-  controls: {
-    number_button: {
-      background: "#FFFFFF",
-      border: "#C5CEE0",
-      progress: "#42AAFF",
-      completed: "#51F0B0",
-    },
-  },
-};
-
-const darkColors = {
-  text: "#FFFFFF",
-  board: {
-    border: "#101426",
-    cell: {
-      normal: "#222B45",
-      peer: "#2E3A59",
-      equal: "#1A2138",
-      conflict: "#FF708D",
-      selected: "#598BFF",
-      completed: "#00B383",
-    },
-  },
-  controls: {
-    number_button: {
-      background: "#101426",
-      border: "#2E3A59",
-      progress: "#598BFF",
-      completed: "#00B383",
-    },
-  },
+const colors = (darkMode: Boolean, theme: Record<string, string>) => {
+  if (darkMode) {
+    return {
+      text: theme["text-basic-color"],
+      board: {
+        border: theme["border-basic-color-4"],
+        cell: {
+          normal: theme["background-basic-color-1"],
+          peer: theme["color-basic-700"],
+          equal: theme["background-basic-color-2"],
+          conflict: theme["color-danger-hover"],
+          selected: theme["color-primary-hover"],
+          completed: theme["color-success-hover"],
+        },
+      },
+      controls: {
+        number_button: {
+          background: theme["background-basic-color-1"],
+          border: theme["background-basic-color-2"],
+          progress: theme["color-primary-hover"],
+          completed: theme["color-success-hover"],
+        },
+      },
+    };
+  } else
+    return {
+      text: theme["text-basic-color"],
+      board: {
+        border: theme["border-alternative-color-4"],
+        cell: {
+          normal: theme["background-basic-color-1"],
+          peer: theme["color-info-200"],
+          equal: theme["color-info-300"],
+          conflict: theme["color-danger-300"],
+          selected: theme["color-warning-200"],
+          completed: theme["color-success-200"],
+        },
+      },
+      controls: {
+        number_button: {
+          background: theme["background-basic-color-1"],
+          border: theme["background-basic-color-4"],
+          progress: theme["color-info-hover"],
+          completed: theme["color-success-hover"],
+        },
+      },
+    };
 };
 
 interface PropTypes {
@@ -61,7 +64,7 @@ const Game: React.FC<PropTypes> = ({ navigation }) => {
   const { /*game,*/ saveGame } = useGame();
   const { settings } = useSettings();
 
-  const light = !settings.app.darkMode;
+  const theme = useTheme();
 
   return (
     <Layout>
@@ -76,7 +79,7 @@ const Game: React.FC<PropTypes> = ({ navigation }) => {
             // board={game.board}
             onChange={saveGame}
             settings={settings.sudoku}
-            colors={light ? lightColors : darkColors}
+            colors={colors(settings.app.darkMode, theme)}
           />
         </Layout>
       </SafeAreaView>
