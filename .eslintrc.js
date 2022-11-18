eeda580f04b785388039420eb12cc900ef585b16
Dawson Booth <dawson@dawsonbooth.com>
2022-11-17 19:43:59 -0600

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,18 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   env: {
     node: true,
     es6: true,
-    "react-native/react-native": true,
+    'react-native/react-native': true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: [
-    "react",
-    "react-hooks",
-    "react-native",
-    "@typescript-eslint",
-    "prettier",
-  ],
+  plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-empty-interface": "off",
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-};
+}
