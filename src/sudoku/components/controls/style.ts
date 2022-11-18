fe0fe25a8aa0d25c5bd204fdb0af406ca2a31c19
Dawson Booth <dawson@dawsonbooth.com>
2022-11-17 22:46:16 -0600

    Update controls layout

--- src/sudoku/components/controls/style.ts
+++ src/sudoku/components/controls/style.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components/native'
 
 export const Wrapper = styled.View<{ isPortrait?: boolean }>`
   flex-flow: row nowrap;
-  justify-content: center;
+  justify-content: space-evenly;
+  width: 100%;
   flex-flow: ${({ isPortrait }) => (isPortrait ? 'row' : 'column')} nowrap;
-  margin: 10px;
+  margin-top: 15px;
+  margin-bottom: 5px;
 `
 // TODO: Replace margin-vertical with flex gap on parent when available:
 // https://github.com/styled-components/styled-components/issues/3628
