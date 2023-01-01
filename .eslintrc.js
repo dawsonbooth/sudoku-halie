bafb7b489daabdb2dcd28dc1e67ebe0810bdc116
Dawson Booth <dawson@dawsonbooth.com>
2022-12-31 19:50:38 -0600

    Use recommended eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,35 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     node: true,
     es6: true,
     'react-native/react-native': true,
   },
-  plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint'],
-  rules: {
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+  ],
   settings: {
     react: {
       version: 'detect',
     },
+  },
+  rules: {
+    '@typescript-eslint/no-empty-interface': 'off',
+    'react/prop-types': 'off',
+    // https://github.com/Intellicode/eslint-plugin-react-native/issues/270
+    'react-native/no-raw-text': 'off',
   },
 }
